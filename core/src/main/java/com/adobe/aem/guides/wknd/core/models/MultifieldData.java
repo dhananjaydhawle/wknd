011d7ef0f89f3cff92ec915c1665a88d42302c17
dhananjaydhawle <dhananjaydhawle16@gmail.com0>
2025-08-20 13:11:40 +0530

    this MultifieldData add mutifield properties

---
+++ core/src/main/java/com/adobe/aem/guides/wknd/core/models/MultifieldData.java
@@ -0,0 +1,25 @@
+package com.adobe.aem.guides.wknd.core.models;
+
+import org.apache.sling.api.SlingHttpServletRequest;
+import org.apache.sling.api.resource.Resource;
+import org.apache.sling.models.annotations.DefaultInjectionStrategy;
+import org.apache.sling.models.annotations.Model;
+import org.apache.sling.models.annotations.injectorspecific.ValueMapValue;
+
+@Model(adaptables = {Resource.class, SlingHttpServletRequest.class}, defaultInjectionStrategy = DefaultInjectionStrategy.OPTIONAL)
+public class MultifieldData {
+
+    @ValueMapValue
+    String path;
+
+    @ValueMapValue
+    String title;
+
+    public String getPath() {
+        return path;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+}
