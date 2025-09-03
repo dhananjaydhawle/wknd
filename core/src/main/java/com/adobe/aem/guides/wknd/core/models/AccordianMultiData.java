11939d37673c2fcc7dadfdae6eabfdf435452809
dhananjaydhawle <dhananjaydhawle16@gmail.com0>
2025-09-03 14:56:42 +0530

    accordian component add one more fiels

---
+++ core/src/main/java/com/adobe/aem/guides/wknd/core/models/AccordianMultiData.java
@@ -0,0 +1,25 @@
+package com.adobe.aem.guides.wknd.core.models;
+
+import org.apache.sling.api.SlingHttpServletRequest;
+import org.apache.sling.api.resource.Resource;
+import org.apache.sling.models.annotations.DefaultInjectionStrategy;
+import org.apache.sling.models.annotations.Model;
+import org.apache.sling.models.annotations.injectorspecific.ValueMapValue;
+
+@Model(adaptables = {Resource.class, SlingHttpServletRequest.class}, defaultInjectionStrategy = DefaultInjectionStrategy.OPTIONAL)
+public class AccordianMultiData {
+
+    @ValueMapValue
+    String question;
+
+    @ValueMapValue
+    String answer;
+
+    public String getQuestion() {
+        return question;
+    }
+
+    public String getAnswer() {
+        return answer;
+    }
+}
---
+++ core/src/main/java/com/adobe/aem/guides/wknd/core/models/AccordianNewModel.java
@@ -0,0 +1,27 @@
+package com.adobe.aem.guides.wknd.core.models;
+
+import org.apache.sling.api.SlingHttpServletRequest;
+import org.apache.sling.api.resource.Resource;
+import org.apache.sling.models.annotations.DefaultInjectionStrategy;
+import org.apache.sling.models.annotations.Model;
+import org.apache.sling.models.annotations.injectorspecific.ChildResource;
+import org.apache.sling.models.annotations.injectorspecific.ValueMapValue;
+
+import java.util.List;
+
+@Model(adaptables = {Resource.class, SlingHttpServletRequest.class}, defaultInjectionStrategy = DefaultInjectionStrategy.OPTIONAL)
+public class AccordianNewModel {
+    @ValueMapValue
+    String title;
+
+    @ChildResource
+    List<AccordianMultiData> bookListMulti;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public List<AccordianMultiData> getBookListMulti() {
+        return bookListMulti;
+    }
+}
